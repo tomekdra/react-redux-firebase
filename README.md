112eae6e1d099986a4abcdf249d90ddff15e61e7
Tom Dra <tomekdrabek@interia.pl>
2020-05-27 18:03:59 +0200

    Initial, files structure

---
+++ README.md
@@ -0,0 +1,3 @@
+## Learn: redux + firebase
+#### Tutorial from The Net Ninja
+* [React, Redux & Firebase App Tutorial](https://www.youtube.com/playlist?list=PL4cUxeGkcC9iWstfXntcj8f-dFZ4UtlN3)
---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom'
+
+function App() {
+    return (
+        <BrowserRouter>
+            <div className="App">
+                hello
+            </div>
+        </BrowserRouter>
+    );
+}
+
+export default App;
